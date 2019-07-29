15442465569e86aba808d966d4020665b7ee5cb1
theblacguy <samueljames@mec.science>
2019-07-29 09:43:19 -0400

    Day 5

---
+++ DAY5/MorningLab/java.js
@@ -0,0 +1,15 @@
+console.log (1);
+console.log (2);
+console.log (3);
+console.log (4);
+console.log (5);
+console.log (6);
+console.log (7);
+console.log (8);
+console.log (9);
+console.log (10);
+
+
+
+
+console.log('1\n2\n3\n4\n5\n6\n7\n8\n9\n10')
